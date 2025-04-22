5f22167db0e056a8c611698691732a0d1e108d7f
Fwolagos <aantonypv@gmail.com>
2025-04-21 19:36:06 -0600

    Feat: Create functions for creditsWindow
    A back function is created which returns you to the mainWindow.

--- BatallaNaval/src/main/java/com/mycompany/batallanaval/CreditsWindowController.java
+++ BatallaNaval/src/main/java/com/mycompany/batallanaval/CreditsWindowController.java
@@ -4,9 +4,17 @@
  */
 package com.mycompany.batallanaval;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -21,6 +29,23 @@ public class CreditsWindowController implements Initializable {
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		// TODO
-	}	
+	}
+
+	@FXML
+	private void back(ActionEvent event) throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("mainWindow.fxml"));
+			Parent root = loader.load(); // Carga el FXML y crea los nodos
+			// GameWindowController controller = loader.getController(); // Obtiene la instancia del controlador
+
+			// Ahora podés pasarle datos al controlador
+					Stage stage = new Stage();
+			stage.setScene(new Scene(root));
+			stage.show();
+
+			Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+			currentStage.close();
+	}
+
 	
+
 }
